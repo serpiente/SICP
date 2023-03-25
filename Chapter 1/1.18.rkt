f872ee64b7c48bc0afec0c9c17042ea32b5226bd
sergio villegas <sergiovillegas@sergios-MacBook-Air.local>
2023-03-25 12:07:04 +0100

    1.18

---
+++ Chapter 1/1.18.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (double x) (* x 2))
+(define (halve x) (/ x 2))
+
+
+(define (iter-mult a b res)
+    (cond((= b 0) res)
+    ((even? b) (iter-mult (double a) (halve b) res))
+    (else (iter-mult a (- b 1) (+ res a)))
+    )
+)
+
+(iter-mult 100 5 0)
